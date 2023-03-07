b61bc8b86e01907f6ef62a176e65e4cf5eb6907d
Julien SOSTHENE <tiesse@pm.me>
2023-03-07 16:14:47 +0100

    Moved to rust-analyzer friendly behaviour

--- reindeer-macros/src/lib.rs
+++ reindeer-macros/src/lib.rs
@@ -1,16 +1,20 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream, TokenTree, Group};
 use proc_macro2::Span;
 use syn::{ItemStruct};
-use quote::{quote,ToTokens};
+use quote::{quote,quote_spanned,ToTokens};
 use syn::Ident;
 use std::fs::{write,create_dir_all};
 use std::path::{Path, PathBuf};
 
 #[proc_macro_attribute]
 pub fn entity(args : TokenStream, item : TokenStream) -> TokenStream {
-    let ast: Result<ItemStruct,syn::Error> = syn::parse(item);
+    let ast: Result<ItemStruct,syn::Error> = syn::parse(item.clone());
     if let Err(e) = ast {
-        panic!("The entity attribute must be applied on a struct.")
+        let mut content : TokenStream = quote! {
+            compile_error!("'entity' can only be applied on a valid struct.");
+        }.to_token_stream().into();
+        content.extend([item]);
+        return content;
     }
     let ast = ast.unwrap();
     let struct_name = ast.ident.clone();
@@ -23,6 +27,6 @@ pub fn entity(args : TokenStream, item : TokenStream) -> TokenStream {
     write(&migrations_dir,"Hello, world!").unwrap();
     quote! {
         #ast
-        pub type #versionned_name = #struct_name;
+        type #versionned_name = #struct_name;
     }.into_token_stream().into()
 }
