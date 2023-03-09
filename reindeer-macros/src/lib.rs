b5c04ffd2817fd6f718cf43c3b2f3824cb5f6210
John Tiesselune <juliensos10@gmail.com>
2023-03-09 08:55:26 +0100

    Some cleanup

--- reindeer-macros/src/lib.rs
+++ reindeer-macros/src/lib.rs
@@ -1,19 +1,19 @@
-use proc_macro::{TokenStream, TokenTree, Group};
+use proc_macro::TokenStream;
 use proc_macro2::Span;
-use syn::{ItemStruct};
-use quote::{quote,quote_spanned,ToTokens};
+use syn::ItemStruct;
+use quote::{quote,ToTokens};
 use syn::Ident;
 use std::fs::{write,create_dir_all};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 #[proc_macro_attribute]
 pub fn entity(args : TokenStream, item : TokenStream) -> TokenStream {
-    let ast: Result<ItemStruct,syn::Error> = syn::parse(item.clone());
-    if let Err(e) = ast {
-        let mut content : TokenStream = quote! {
+    let ast: Result<ItemStruct,syn::Error> = syn::parse(item);
+    if let Err(_) = ast {
+        let content : TokenStream = quote! {
             compile_error!("'entity' can only be applied on a valid struct.");
         }.to_token_stream().into();
-        content.extend([item]);
+        //content.extend([item]);
         return content;
     }
     let ast = ast.unwrap();
@@ -21,6 +21,7 @@ pub fn entity(args : TokenStream, item : TokenStream) -> TokenStream {
     let versionned_name = Ident::new(&format!("{}_v{}",struct_name,1), Span::call_site());
     let dir = env!("CARGO_MANIFEST_DIR");
     let mut migrations_dir = PathBuf::new();
+    migrations_dir.push(dir);
     migrations_dir.push("schema");
     create_dir_all(&migrations_dir).unwrap();
     migrations_dir.push(&format!("{}_v{}.json",struct_name,1));
--- reindeer/src/tests/mod.rs
+++
@@ -1,9 +0,0 @@
-use super::*;
-
-use reindeer_macros::entity;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
