aef4ca948320ee4443ac6f32a984512021539522
Julien SOSTHENE <tiesse@pm.me>
2023-03-10 15:53:54 +0100

    Generics taken into account

--- reindeer-macros/src/lib.rs
+++ reindeer-macros/src/lib.rs
@@ -139,14 +139,14 @@ pub fn derive_entity(item : TokenStream) -> TokenStream {
 
 fn construct_token_stream(input : &DeriveInput, errors : &mut Errors) -> TokenStream {
     let mut result = TokenStream::new();
-    
+
     match &input.data {
         syn::Data::Struct(s) => {
             let entity_data = EntityData::parse(&input.span(),&input.attrs,&s.fields, errors);
             let attr_copy = entity_data.clone();
             result.extend([
-                generate_alias(&input.ident, entity_data.version.unwrap_or(0), &input.vis),
-                generate_impl( &input.ident, &attr_copy),
+                generate_alias(&input.ident, entity_data.version.unwrap_or(0), &input.vis, &input.generics),
+                generate_impl( &input.ident, &attr_copy, &input.generics),
             ])
         },
         syn::Data::Enum(_) => errors.push(syn::Error::new_spanned(input, "Cannot derive Entity on an enum. Please implement Entity manually.")),
@@ -155,14 +155,15 @@ fn construct_token_stream(input : &DeriveInput, errors : &mut Errors) -> TokenSt
     result
 }
 
-fn generate_alias(name : &Ident,version : u32, vis : &Visibility) -> TokenStream {
+fn generate_alias(name : &Ident,version : u32, vis : &Visibility, generics : &syn::Generics) -> TokenStream {
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
     let versionned_ident = Ident::new(&format!("{}_v{}",name.to_string(),version), Span::call_site());
     quote ! {
-        #vis type #versionned_ident = #name;
+        #vis type #versionned_ident #ty_generics = #name #ty_generics;
     }.into()
 }
 
-fn generate_impl(struct_name : &Ident,entity_data : &EntityData) -> TokenStream {
+fn generate_impl(struct_name : &Ident,entity_data : &EntityData, generics : &syn::Generics) -> TokenStream {
 
     if let (Some(store_name),Some(id_field),Some(key_type),crate_name) = (&entity_data.name,&entity_data.id,&entity_data.id_type,&entity_data.crate_name) {
         let crate_name = Ident::new(crate_name,Span::call_site());
@@ -174,8 +175,9 @@ fn generate_impl(struct_name : &Ident,entity_data : &EntityData) -> TokenStream 
             let (name,deletion) = (e.0.clone(),e.1.clone());
             quote!{(#name,#crate_name::DeletionBehaviour::#deletion)}
         }).collect();
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
         quote!{
-            impl #crate_name::Entity for #struct_name {
+            impl #impl_generics #crate_name::Entity for #struct_name #ty_generics #where_clause {
                 type Key = #key_type;
                 fn store_name() -> &'static str {
                     #store_name
